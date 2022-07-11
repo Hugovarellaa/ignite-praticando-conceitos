e31da457073a469d8dbcd3e6dbdb1d2d8f232066
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 <hugovarellaa@gmail.com>
2022-07-11 20:18:32 -0300

    Copiando o Front-end para o Back-end

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -6,10 +6,11 @@ const app = express();
 
 // Setando o view engine
 app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "../public")));
 
 // Configurando o diretório das views
 app.set("views", path.join(__dirname, "./views"));
 
 app.use(route);
 
-app.listen(3333, () => console.log("Server started on port 3333"));
+app.listen(3000, () => console.log("Server started on port 3333"));
