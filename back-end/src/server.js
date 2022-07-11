9e5775e8d36757ab423accbfc18c94b18697a055
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 <hugovarellaa@gmail.com>
2022-07-11 18:07:34 -0300

    Configurando EJS

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const route = require("./route");
+const path = require("path");
 
 const app = express();
+
+// Setando o view engine
+app.set("view engine", "ejs");
+
+// Configurando o diretório das views
+app.set("views", path.join(__dirname, "./views"));
+
 app.use(route);
 
 app.listen(3333, () => console.log("Server started on port 3333"));
