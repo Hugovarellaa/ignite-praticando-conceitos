068ac8f4b5a3d64334eeaab6c7b60176bb4d1919
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 <hugovarellaa@gmail.com>
2022-07-11 20:28:00 -0300

    Adicionando JS

--- back-end/src/route.js
+++ back-end/src/route.js
@@ -2,8 +2,10 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.render("index");
-});
+router.get("/", (req, res) => res.render("index"));
+
+router.get("/create-pass", (req, res) => res.render("create-pass"));
+
+router.get("/room", (req, res) => res.render("room"));
 
 module.exports = router;
--- back-end/src/server.js
+++ back-end/src/server.js
@@ -6,6 +6,8 @@ const app = express();
 
 // Setando o view engine
 app.set("view engine", "ejs");
+
+// Setando o diretório dos Styles e Scripts
 app.use(express.static(path.join(__dirname, "../public")));
 
 // Configurando o diretório das views
@@ -13,4 +15,4 @@ app.set("views", path.join(__dirname, "./views"));
 
 app.use(route);
 
-app.listen(3000, () => console.log("Server started on port 3333"));
+app.listen(3000, () => console.log("Server started on port 3000"));
