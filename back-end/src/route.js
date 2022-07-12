475fe1f8c29d900641870ed0ea81a261a554ec17
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 <hugovarellaa@gmail.com>
2022-07-11 22:27:44 -0300

    #part 3 2:00:00

--- back-end/src/route.js
+++ back-end/src/route.js
@@ -3,9 +3,11 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => res.render("index"));
-
 router.get("/create-pass", (req, res) => res.render("create-pass"));
-
 router.get("/room", (req, res) => res.render("room"));
+
+router.get("/room/:room/:question/:actions", (req, res) =>
+  res.render("exemplo", { req })
+);
 
 module.exports = router;
