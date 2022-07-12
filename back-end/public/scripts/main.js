00ca6f60bfbee4442cc4aa08cdebd7e55ea06220
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 <hugovarellaa@gmail.com>
2022-07-11 21:52:25 -0300

    2 Modais diferentes

--- back-end/public/scripts/main.js
+++ back-end/public/scripts/main.js
@@ -2,18 +2,29 @@ import Modal from "./modal.js";
 
 const modal = Modal();
 
+const modalTitle = document.querySelector(".modal h2");
+const modalDescription = document.querySelector(".modal p");
+const modalButton = document.querySelector(".modal button");
+
 const checkButton = document.querySelectorAll(".actions a.check");
 
+//Marca como lidar
 checkButton.forEach((button) => {
   button.addEventListener("click", () => {
+    modalTitle.innerHTML = "Marca como lidar";
+    modalDescription.innerHTML = "Deseja marcar como lidar com este item?";
     modal.open();
   });
 });
 
 const deleteButton = document.querySelectorAll(".actions a.delete");
 
+//Excluir item
 deleteButton.forEach((button) => {
   button.addEventListener("click", () => {
+    modalTitle.innerHTML = "Excluir pergunta";
+    modalDescription.innerHTML =
+      "Tem certeza que você deseja excluir esta pergunta?";
     modal.open();
   });
 });
