6671f7dfbc51db00da7b74eb466ae07fe0ab20f9
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-07-13 09:27:50 -0300

    Delete ignite-timer directory

--- ignite-timer/ignite-timer/src/@types/styles.d.ts
+++
@@ -1,7 +0,0 @@
-import "styled-components";
-import { defaultTheme } from "../styles/themes/default";
-
-declare module "styled-components" {
-  type ThemeType = typeof defaultTheme;
-  export interface DefaultTheme extends ThemeType {}
-}
