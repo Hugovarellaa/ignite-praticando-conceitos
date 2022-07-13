758a946e799010c67cffa47af8a599a9e1f0f8b3
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 <hugovarellaa@gmail.com>
2022-07-13 10:33:44 -0300

    Estrutura da aplicacao

--- ignite-timer/src/styles/global/GlobalStyles.ts
+++ ignite-timer/src/styles/global/GlobalStyles.ts
@@ -7,12 +7,21 @@ export const GlobalStyles = createGlobalStyle`
     box-sizing: border-box;
   }
 
+  :focus {
+    outline: 0;
+    box-shadow: 0 0 0 2px ${({ theme }) => theme['green-500']};
+  }
+
+  body {
+    background: ${({ theme }) => theme['gray-900']};
+    color: ${({ theme }) => theme['gray-300']};
+  }
+
   body, input, textarea, button, select {
     font-family: 'Roboto', sans-serif;
     font-size: 1rem;
     font-weight: 400;
-    background: ${({ theme }) => theme['gray-900']};
-    color: ${({ theme }) => theme['gray-300']};
+    
   }
 
 `
